96cad195e75dea93b5bec301b4d01833c8f40cbb
RyanGarfinkel <ryanpgarfinkel@knights.ucf.edu>
2023-03-02 11:22:06 -0500

    Updated content.js
    Checked if num1 or num2 are invalid and added the modulo operator

--- content.js
+++ content.js
@@ -5,6 +5,28 @@ chrome.browserAction.onClicked.addListener(function(tab)
     chrome.tabs.create({ url: "https://www.example.com" });
 });
 
+function eval(num1, num2, op)
+{
+    if(num1 == "Error" || num2 == "Error")
+        return "Error";
+    
+    switch(op)
+    {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        case "%":
+            return modulo(num1, num2);
+        default:
+            return "Error";
+    }
+}
+
 function add(num1, num2)
 {
     return num1 + num2;
@@ -22,6 +44,17 @@ function multiply(num1, num2)
 
 function divide(num1, num2)
 {
+    if(num2 == 0)
+        return "Erorr";
+
     return num1 / num2;
+}
+
+function modulo(num1, num2)
+{
+    if(num2 == 0)
+        return "Error";
+
+    return num1 % num2;
 }
   
