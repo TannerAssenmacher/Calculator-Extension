9a661600fedf807fe1846ed0bef26861bb06b856
RyanGarfinkel <ryanpgarfinkel@knights.ucf.edu>
2023-03-02 11:38:23 -0500

    Updated content.js
    Created methods for calculating the result, clearing the display, and removing the last digit of the current value on display.

--- content.js
+++ content.js
@@ -1,60 +1,56 @@
+let label = document.getElementById('display');
+
 // Add a listener for the browser action button
 chrome.browserAction.onClicked.addListener(function(tab) 
 {
     // Create a new tab with a specific URL
-    chrome.tabs.create({ url: "https://www.example.com" });
+    chrome.tabs.create({ url: 'https://www.example.com' });
 });
 
-function eval(num1, num2, op)
+function display(num)
+{   
+    label.innerHTML = label.text + num;
+}
+
+function Clear()
 {
-    if(num1 == "Error" || num2 == "Error")
-        return "Error";
+    label.innerHTML = '';
+}
+
+function del()
+{
+    let curr = label.innerHTML;
+
+    if(curr.length == 0)
+        return;
+    else if(curr.length == 1)
+        label.innerHTML = '';
+
+    label.innerHTML = curr.slice(0, curr.length - 1);
+}
+
+function Calculate(num1, num2, op)
+{
+    if(num1 == 'Error' || num2 == 'Error')
+        return 'Error';
     
     switch(op)
     {
-        case "+":
-            return add(num1, num2);
-        case "-":
-            return subtract(num1, num2);
-        case "*":
-            return multiply(num1, num2);
-        case "/":
-            return divide(num1, num2);
-        case "%":
-            return modulo(num1, num2);
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            if(num2 == 0)
+                return 'Error';
+            return num1 / num2;
+        case '%':
+            if(num2 == 0 || !Number.isInteger(num1 / num2))
+                return 'Error';
+            return num1 % num2;
         default:
-            return "Error";
+            return 'Error';
     }
 }
-
-function add(num1, num2)
-{
-    return num1 + num2;
-}
-
-function subtract(num1, num2)
-{
-    return num1 - num2;
-}
-
-function multiply(num1, num2)
-{
-    return num1 * num2;
-}
-
-function divide(num1, num2)
-{
-    if(num2 == 0)
-        return "Erorr";
-
-    return num1 / num2;
-}
-
-function modulo(num1, num2)
-{
-    if(num2 == 0)
-        return "Error";
-
-    return num1 % num2;
-}
-  
