454770309739519b86e301b2ee9e966d54d40f90
promise310 <seoyoung22dev@gmail.com>
2023-04-06 12:24:32 -0400

    Added icon

--- js/mode.js
+++ js/mode.js
@@ -1,9 +1,26 @@
-const themeToggleBtn = document.querySelector('.theme-toggler');
-const calculator = document.querySelector('.calculator');
-const toggleIcon = document.querySelector('.toggler-icon');
-let isDark = true;
-themeToggleBtn.onclick = () => {
-    calculator.classList.toggle('dark');
-    themeToggleBtn.classList.toggle('active');
-    isDark = !isDark;
-}
+const toggle = document.getElementById('theme-toggler')
+const body = document.querySelector('body');
+// const themeToggleBtn = document.querySelector('.theme-toggler');
+// const calculator = document.querySelector('.calculator');
+
+
+// const toggleIcon = document.querySelector('.toggler-icon');
+// let isDark = true;
+// themeToggleBtn.onclick = () => {
+//     calculator.classList.toggle('dark');
+//     themeToggleBtn.classList.toggle('active');
+//     isDark = !isDark;
+// }
+
+toggle.addEventListner('click', function(){
+    this.classList.toggle('far fa-moon');
+    if(this.classList.toggle('fas fa-moon')){
+        body.style.background = 'white';
+        body.style.color = 'black'
+        body.style.transition = '1s';
+    }else {
+        body.style.background = 'black';
+        body.style.color = 'white'
+        body.style.transition = '1s';
+    }
+})
