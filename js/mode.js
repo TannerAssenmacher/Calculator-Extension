67cc798bed0c7341f22aefcfbc893d00d8200951
promise310 <seoyoung22dev@gmail.com>
2023-04-18 01:17:46 -0400

    Updated CSS/mode.js

--- js/mode.js
+++ js/mode.js
@@ -8,14 +8,14 @@ toggle.addEventListener('click', function()
     if(this.classList.toggle('bi-brightness-high-fill'))
     {
         calculator.style.background = 'white';
-        toggle.style.color = 'black';
+        toggle.style.color = '#FF5F1F';
         content.style.color = 'black';
         calculator.style.transition = '1.5s';
     }
     else 
     {
         calculator.style.background = 'black';
-        toggle.style.color = 'white';
+        toggle.style.color = '#FF5F1F';
         content.style.color = 'white';
         calculator.style.transition = '1.5s';
     }
